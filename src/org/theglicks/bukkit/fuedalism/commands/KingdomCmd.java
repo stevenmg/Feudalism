b0ec06b1588acd4e8a7fa4ee6177fbe837a31be9
Steven Glick <steven@theglicks.org>
2013-06-18 15:42:22 -0400

    Added basic kingdom commands

--- src/org/theglicks/bukkit/fuedalism/commands/KingdomCmd.java
+++ src/org/theglicks/bukkit/fuedalism/commands/KingdomCmd.java
@@ -5,13 +5,24 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.theglicks.bukkit.fuedalism.Fuedalism;
+import org.theglicks.bukkit.fuedalism.kingdoms.Kingdom;
 
 public class KingdomCmd implements CommandExecutor{
 	public KingdomCmd(Fuedalism fuedalism) {}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(sender instanceof Player){
-			
+			Player player = (Player) sender;
+			if(args[0].equalsIgnoreCase("create")){
+				Kingdom.createKingdom(args[1], sender.getName());
+			} else if(args[0].equalsIgnoreCase("vassals")){
+				Kingdom k = new Kingdom(args[1]);
+				StringBuilder builder = new StringBuilder();
+				for(String name: k.getMembers()){
+					builder.append(name);
+				}
+				player.sendMessage(builder.toString());
+			}
 		}
 		return true;
 	}

4efff6bf6f44815b9c7c4798d356a646a8b4a454
Steven Glick <steven@theglicks.org>
2013-08-06 10:52:56 -0400

    Added damage blocking

--- src/org/theglicks/bukkit/fuedalism/Fuedalism.java
+++ src/org/theglicks/bukkit/fuedalism/Fuedalism.java
@@ -5,6 +5,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.theglicks.bukkit.fuedalism.commands.FiefCmd;
 import org.theglicks.bukkit.fuedalism.commands.KingdomCmd;
 import org.theglicks.bukkit.fuedalism.events.BlockBreak;
+import org.theglicks.bukkit.fuedalism.events.BlockPlace;
+import org.theglicks.bukkit.fuedalism.events.EntityDamageByEntity;
+import org.theglicks.bukkit.fuedalism.events.InventoryOpen;
 import org.theglicks.bukkit.fuedalism.events.PlayerInteract;
 import org.theglicks.bukkit.fuedalism.events.PlayerMove;
 
@@ -24,6 +27,9 @@ public class Fuedalism extends JavaPlugin{
 		getServer().getPluginManager().registerEvents(new PlayerInteract(), this);
 		getServer().getPluginManager().registerEvents(new PlayerMove(), this);
 		getServer().getPluginManager().registerEvents(new BlockBreak(), this);
+		getServer().getPluginManager().registerEvents(new BlockPlace(), this);
+		getServer().getPluginManager().registerEvents(new InventoryOpen(), this);
+		getServer().getPluginManager().registerEvents(new EntityDamageByEntity(), this);
 		
 		getCommand("kingdom").setExecutor(new KingdomCmd(this));
 		getCommand("fief").setExecutor(new FiefCmd(this));
---
+++ src/org/theglicks/bukkit/fuedalism/events/EntityDamageByEntity.java
@@ -0,0 +1,27 @@
+package org.theglicks.bukkit.fuedalism.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.theglicks.bukkit.fuedalism.Vassal;
+import org.theglicks.bukkit.fuedalism.kingdoms.Kingdom;
+
+public class EntityDamageByEntity implements Listener {
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent event){
+		if(event.getDamager() instanceof Player && event.getEntity() instanceof Player){
+			Player damager = (Player) event.getDamager();
+			Player damagee = (Player) event.getEntity();
+			Vassal vDamager = new Vassal(damager.getName());
+			Vassal vDamagee = new Vassal(damagee.getName());
+			Kingdom damagerKingdom = vDamager.getKingdom();
+			Kingdom damageeKingdom = vDamagee.getKingdom();
+			
+			if(damagerKingdom == damageeKingdom || vDamager.isAllied(vDamagee)){
+				event.setCancelled(true);
+				damager.sendMessage("You cannot hit that player!");
+			}
+		}
+	}
+}
